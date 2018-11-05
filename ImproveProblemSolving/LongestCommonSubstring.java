a1a0a9a042c3c2f67c7960f21453088011fd71b3
Alicewillbe <zhaolonglong4008@gmail.com>
2018-11-05 12:16:11 -0800

    Update LongestCommonSubstring.java

--- ImproveProblemSolving/LongestCommonSubstring.java
+++ ImproveProblemSolving/LongestCommonSubstring.java
@@ -30,7 +30,7 @@
 *           hidden loop kills people 
 */
 public class LongestCommonSubstring {
-	public static String solution(String s1, String s2) {
+	public static String poorSolution(String s1, String s2) {
 		String reuslt = "";
 		for (int i = 0; i < s1.length(); i++) {
 			// bug here if you want to use substring
@@ -45,5 +45,71 @@ public class LongestCommonSubstring {
 			}
 		}
 		return result;
+	}
+	
+	public static String bugFixedNotOptimal(String s1, String s2) {
+		String reuslt = "";
+		for (int i = 0; i < s1.length(); i++) {
+			// in substring, we want range [i, j] be valid
+			// i < j to ensure no empty string case
+			// j is initially s1.length() to cover all cases
+			for (int j = s1.length(); j > i; j--) {
+				if (s2.contains(s1.substring(i,j))) {
+					if (result.length() < s1.substring(i,j).length()) {
+						result = s1.substring(i,j);
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	// The big flaw during this interview is that I start coding
+	// without thinking too much.
+	// The very first thing I should do is to make sure I understand
+	// the question and already have several high level solution to begin with
+	// confirmed with interviewers, and start coding at once
+	
+	// Big picture:
+	// Question: longest_common_subseq
+	// Input: String s1, String s2
+	// Output: String - null if one is null
+	//                - "" if no common subseq
+	//                - found some subseq
+	//
+	// Tests:
+	// str1       str2        expect
+	// "abvcd"    "abcd"      "ab"
+	// "abcd"     "abcd"      "abcd"
+	// some null		  null
+	
+	// Questions:
+	// 1. duplicate answers, which to return?
+	// 2. null case?
+	// 3. what characterize a good solution?
+	
+	// Soln1: get all possible substrings from 1 input
+	//	  check whether substring is contained in
+	//        the other input
+	// Soln2: get all possible substrings from the shorter input
+	//        check whether they are contained in the longer input
+	//        better if length of substring is in decreasing order
+	
+	public static String solution(String s1, String s2) {
+		if (s1 == null || s2 == null) return null;
+		if (s1.isEmpty() || s2.isEmpty()) return "";
+		if (s1.length() > s2.length()) return solution(s2,s1);
+		
+		for (int i = s1.length(); i >= 1; i--) { // O(n)
+			for (int j = 0; j < s1.length() + 1 - i; j++) { // worst case O(n)
+				// take out the valid substring
+				String curr = s1.substring(j, j + i);
+				if (s2.contains(curr)) {  // O(i)
+					return curr;
+				}
+			}
+		}
+		
+		return ""; // not found
 	}
 }
